90c980bdcc1e70fa5e6ba17e7ee19e9fd94953ec
Zl0bster <s00per@ya.ru>
2020-06-12 22:30:20 +0300

    initial commit

---
+++ os_info.py
@@ -0,0 +1,13 @@
+# get OS info
+# get python version info
+
+import platform
+import sys
+
+info = 'OS info is \n {} \n\nPython version is {} {}'.format(
+    platform.uname(), sys.version, platform.architecture())
+
+print(info)
+
+with open('os_info.txt', 'w') as ff:
+    ff.write(info)
