75a9575d435f542ffb08e75a119b4c67f42c5459
Zl0bster <s00per@ya.ru>
2020-07-10 00:50:54 +0300

    added decorator with calls calculation decorator does not work with generator correctly

--- brut_gen.py
+++ brut_gen.py
@@ -2,9 +2,24 @@
 #
 # creates sequence generator of combinations of defined length
 
-def seq_gen(length=3, alphabet='0123456789'):
-    # generates sequence of combinations of defined length
+def fn_calls_counter(func):
+    """decorator counts number of function calls"""
+    fn_calls_counter.count = 0
+
+    def wrapper(*args, **kwargs):
+        fn_calls_counter.count += 1
+        result = func(*args, **kwargs)
+        print("{0} was called: {1}".format(func.__name__, fn_calls_counter.count))
+        return result
+
+    return wrapper
+
+
+@fn_calls_counter
+def seq_gen(length=3, alphabet='0123456789') -> str:
+    """generates sequence of combinations of defined length using alphabet"""
     base = len(alphabet)
+    total_count = 0
     for current_length in range(1, length + 1):
         combination_count = base ** current_length
         for n in range(0, combination_count):
@@ -21,12 +36,19 @@ def seq_gen(length=3, alphabet='0123456789'):
                 leading = ''
             combination = leading + combination
             yield combination
-    return
+            total_count += 1
+    print('total count = {}'.format(total_count))
+    return total_count
 
 
+# counted_gen = fn_calls_counter(seq_gen)
+# gen = counted_gen(3, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+
+gen = seq_gen(4, '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 try:
-    gen = seq_gen(3, '0123456789ABCDEF')
     while True:
         print(next(gen))
 except StopIteration:
     print('end iteration')
+finally:
+    print('the end')
