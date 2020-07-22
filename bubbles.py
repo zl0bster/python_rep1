02e0847c2ce894734db968f8d68606894ebb7077
Zl0bster <s00per@ya.ru>
2020-07-22 17:38:46 +0300

    added class methods definitions and class attributes get-set methods

--- bubbles.py
+++ bubbles.py
@@ -1,6 +1,7 @@
 import simple_draw as sd
 from win32api import GetSystemMetrics
 import fractal_tree_draw as fd
+import transform_decart_ang as da_trans
 
 
 class Screen:
@@ -13,30 +14,44 @@ class Screen:
         self.x_resolution = x_size if x_size < width else width
         self.y_resolution = y_size if y_size < height else height
         self.mobile_objects_count = 0
-        self.stationary_object_count = 0
+        self.static_objects_count = 0
         self.mobile_objects = []
-        self.stationary_objects = []
+        self.static_objects = []
         sd.resolution = (self.x_resolution, self.y_resolution)
-        # TODO change tree roots coordinates
-        fd.fractal_tree(sd.get_point(800, 500), 200, 275, 40, 0.6, )
-        fd.fractal_tree(sd.get_point(500, 200), 150, 120, 30, 0.65, sd.COLOR_DARK_ORANGE)
-        #
+        self.draw_screen_background()
         sd.take_background()
-        return self.x_resolution, self.y_resolution
+        return
 
     def draw_screen_background(self):
-        # TODO draw all fractals here
-        pass
+        fd.fractal_tree(sd.get_point(int(self.x_resolution * 0.4),
+                                     int(self.y_resolution * 0.25)),
+                        200, 275, 40, 0.6, )
+        fd.fractal_tree(sd.get_point(int(self.x_resolution * 0.6),
+                                     int(self.y_resolution * 0.3)),
+                        150, 120, 30, 0.65, sd.COLOR_DARK_ORANGE)
+        return
 
     def add_mobile_item(self, mov_item):
-        # TODO add item to list
-
-        return
+        if isinstance(mov_item, MobileObject):
+            self.mobile_objects_count += 1
+            self.mobile_objects.append(mov_item)
+            return self.mobile_objects_count
+        return None
 
     def add_stationary_item(self, stat_item):
-        # TODO add item to list
+        if isinstance(stat_item, ScreenObject) and not isinstance(stat_item, MobileObject):
+            self.static_objects_count += 1
+            self.static_objects.append(stat_item)
+            return self.static_objects_count
+        return None
 
-        return
+    def move_mobile_items(self):
+        # TODO move all mobile items
+        pass
+
+    def manage_mobile_items_colisions(self):
+        # TODO check contact of all mobile items with mobile and static items
+        pass
 
     def draw_items(self):
         sd.take_background()
@@ -51,8 +66,8 @@ class Screen:
 
 
 class ScreenObject:
-    """ Has initial point coordinates
-    can be drawn"""
+    """ Has initial point coordinates, reference of own center and dimensions
+    can be drawn with defined color and width"""
 
     def __init__(self, reference: list, relation: list, dimensions: list):
         self.xPosition = reference[0]
@@ -61,6 +76,8 @@ class ScreenObject:
         self.yRelation = relation[1]
         self.xDimension = dimensions[0]
         self.yDimension = dimensions[1]
+        self.color = sd.COLOR_YELLOW
+        self.width = 1
         return
 
     def draw_item(self):
@@ -75,6 +92,16 @@ class ScreenObject:
         r180degrees = self.xRelation
         r270degrees = self.yRelation
         return r000degrees, r090degrees, r180degrees, r270degrees
+
+    def set_color(self, color=None):
+        if color:
+            self.color = color
+        return
+
+    def set_width(self, width=None):
+        if width:
+            self.width = width
+        return
 
 
 class MobileObject(ScreenObject):
@@ -97,17 +124,26 @@ class MobileObject(ScreenObject):
         return
 
     def check_contact(self, opponent: ScreenObject):
+        # TODO return True or False if contact detected
         pass
+
+    def make_movement(self):
+        positionRelation = da_trans.angular_to_decart(distance=self.speedValue, angle=self.speedDirection)
+        self.xPosition += positionRelation[0]
+        self.yPosition += positionRelation[1]
+        return
 
 
 class Block(ScreenObject):
-    #TODO finalize Block class definition, pprovide sides preparation and drawing
-    def __init__(self, reference: list,  dimensions: list):
+    """ rectangular static blocks """
+
+    def __init__(self, reference: list, dimensions: list):
         relation = [0, 0]
         super().__init__(reference, relation, dimensions)
-
+        self.referencePoint = sd.get_point(x=self.xPosition, y=self.yPosition)
+        self.oppositePoint = sd.get_point(x=self.xPosition + self.xDimension, y=self.yPosition + self.yDimension)
+        return
 
     def draw_item(self):
-        referencePoint = sd.get_point(x=self.xPosition, y=yPosition)
-        if self.xDimension>0:
-        branch = sd.get_vector(start_point=referencePoint, angle=angle, length=length, )
+        sd.rectangle(left_bottom=self.referencePoint, right_top=self.oppositePoint, color=self.color, width=1)
+        return
